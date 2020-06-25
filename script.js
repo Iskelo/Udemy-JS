6d1f5f277d85e25606432d0924ce32471b30c667
Isk3lo <iskelo.kap@gmail.com>
2020-06-25 18:32:35 +0200

    Commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hej!!!");
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies:{},
+	actors:{},
+	genres:[],
+	privat: false
+};
